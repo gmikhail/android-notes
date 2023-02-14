508359b4a01ff5027d6e8c3dd7e19306715a1c17
Mikhail Gribanov <mikhail.gribanov@gmail.com>
2023-02-14 14:57:33 +0200

    Add to MainFragment viewBinding and menu

--- app/src/main/java/gmikhail/notes/MainFragment.kt
+++ app/src/main/java/gmikhail/notes/MainFragment.kt
@@ -5,7 +5,44 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import gmikhail.notes.databinding.FragmentMainBinding
 
 class MainFragment : Fragment(R.layout.fragment_main) {
 
+    private var viewBinding: FragmentMainBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        viewBinding = FragmentMainBinding.inflate(inflater, container, false)
+        return viewBinding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewBinding?.topAppBar?.let { toolbar ->
+            toolbar.inflateMenu(R.menu.main_menu)
+            toolbar.setOnMenuItemClickListener {
+                when(it.itemId){
+                    R.id.action_change_display_mode -> {
+                        // TODO recyclerView change display mode
+                        true
+                    }
+                    R.id.action_settings -> {
+                        // TODO navigate to settings
+                        true
+                    }
+                    else -> false
+                }
+
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        viewBinding = null
+    }
 }
