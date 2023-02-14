c9eee757c56873ec8c58ba9e8eb3ca92713bf729
Mikhail Gribanov <mikhail.gribanov@gmail.com>
2023-02-14 13:00:19 +0200

    Add MainFragment

--- app/src/main/java/gmikhail/notes/MainActivity.kt
+++ app/src/main/java/gmikhail/notes/MainActivity.kt
@@ -2,10 +2,17 @@ package gmikhail.notes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<MainFragment>(R.id.fragment_container_view)
+            }
+        }
     }
 }
---
+++ app/src/main/java/gmikhail/notes/MainFragment.kt
@@ -0,0 +1,11 @@
+package gmikhail.notes
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class MainFragment : Fragment(R.layout.fragment_main) {
+
+}
